d60eb7cbfee779eb20cc6ff9983aeacc2097d040
gonzo <krsticm@ymail.com>
2019-04-27 23:43:25 +0200

    survey

--- client/src/app/survey/survey.ts
+++ client/src/app/survey/survey.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from '../services/api.service';
 import {UserService} from '../services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'page-survey',
@@ -16,7 +17,8 @@ export class SurveyPage implements OnInit {
 
 
     constructor(private api: ApiService,
-                private userSerice: UserService) {
+                private userSerice: UserService,
+                private router: Router) {
     }
 
     public async ngOnInit() {
@@ -34,6 +36,11 @@ export class SurveyPage implements OnInit {
     }
 
     private finishSurvey() {
+        this.api.finishSurvey(this.loggedUser.id)
+            .subscribe(
+                this.onFinishSurveySuccess.bind(this),
+                this.onFinishSurveyFailure.bind(this)
+            );
 
     }
 
@@ -43,11 +50,39 @@ export class SurveyPage implements OnInit {
         this.slides.lockSwipes(true);
     }
 
-    private selectAnswer(answer) {
+    private selectAnswer(answer, value) {
         this.hasAnswered = true;
+        this.api.submitSurveyQuestion(this.loggedUser.id, answer.id, value)
+            .subscribe(this.onSubmitQuestionSuccess.bind(this),
+                this.onSubmitQuestionFailure.bind(this));
+    }
+
+    private async onGetUserInfoSuccess(user) {
+        await this.userSerice.setLoggedUser(user.data);
+        this.router.navigateByUrl('home/search');
+
+    }
+
+    private async onGetUserInfoFailure() {
+        console.log('onGetUserInfoFailure');
+    }
+
+    private onFinishSurveySuccess() {
+        this.api.getUserInfo(this.loggedUser.id).subscribe(this.onGetUserInfoSuccess.bind(this));
+    }
+
+    private onFinishSurveyFailure() {
+        console.log('onFinishSurveyFailure');
+    }
+
+    private onSubmitQuestionSuccess(data) {
         setTimeout(() => {
             this.hasAnswered = false;
             this.nextSlide();
-        }, 1000);
+        }, 500);
+    }
+
+    private onSubmitQuestionFailure() {
+        console.log('onSubmitQuestionFailure');
     }
 }
