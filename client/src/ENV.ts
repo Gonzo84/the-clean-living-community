b75c714c2be177afe759e00e10073c907e7019ad
gonzo <krsticm@ymail.com>
2019-04-29 22:11:28 +0200

    env

--- client/src/ENV.ts
+++ client/src/ENV.ts
@@ -1,6 +1,6 @@
 const ENV = {
-    SERVER_ADDRESS: 'http://localhost:9000',
-    SOCKET_ADDRESS: 'http://192.168.10.10:3000',
+    SERVER_ADDRESS: 'http://165.227.53.211:9000',
+    SOCKET_ADDRESS: 'http://165.227.53.211:3000',
 };
 
 export default ENV;
